b12f69bcb4ee274d891c19889b833004f38589f1
Josh Long <josh@joshlong.com>
2017-08-28 17:01:54 +0800

    adding a release blog

--- deploy.sh
+++ deploy.sh
@@ -27,6 +27,27 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin source
-git subtree push --prefix=output git@github.com:joshlong/joshlong.github.io.git master
+
+# git subtree push --prefix=output git@github.com:joshlong/joshlong.github.io.git master
+
+git push origin `git subtree split --prefix output source`:master --force
 
 echo "the page is available now as http://github.com/joshlong/joshlong.github.io.git"
+
+
+
+#//////////////////
+#!/bin/bash
+
+echo -e "deploying updates to Github.."
+hugo
+cp CNAME public/CNAME
+git add public/CNAME
+git commit -a -m "adding CNAME"
+git add -A
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push origin source
